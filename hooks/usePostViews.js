0f2823ed8c81cd0b727cc603d2fa725c9b39493b
Yuri Cunha <isyuricunha@icloud.com>
2022-06-05 05:11:49 -0300

    remove all

--- hooks/usePostViews.js
+++
@@ -1,9 +0,0 @@
-import useSWR from 'swr';
-import fetcher from '@/lib/fetcher';
-
-export const usePostViews = (slug) => {
-  const { data } = useSWR(`/api/views/${slug}`, fetcher);
-  return {
-    views: data?.total
-  };
-};
