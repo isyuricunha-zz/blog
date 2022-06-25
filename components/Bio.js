31205bdf3f90b2fcf9dc999eae8dd7cfea5dd89c
Yuri Cunha <isyuricunha@icloud.com>
2022-06-25 17:02:58 -0300

    Revert "."
    This reverts commit 8a88bfd40a9d684f2831e73e82992d889303c0ac.

--- components/Bio.js
+++ components/Bio.js
@@ -1,41 +1,24 @@
 import Typing from './Typing';
 
 const TYPING_MSGS = [
-<<<<<<< HEAD
   "I'm Fullstack Developer.",
   "I'm Technology Evangelist.",
   "I'm <strong>me<strong>."
-=======
-  'Eu sou um <strong>Administrador de Banco de Dados</strong>',
-  'Amante de <strong>Open-Source</strong> 💙',
-  'Só mais um cara tentando apressar a fila do mercado com o poder da mente',
-  'Definitivamente nascido na terra, e não no quadrante <strong>gamma-7</strong>'
->>>>>>> parent of 0f2823e (remove all)
 ];
 
 const Bio = () => {
   const options = {
     strings: TYPING_MSGS,
     typeSpeed: 65,
-<<<<<<< HEAD
     backSpeed: 70,
     cursorChar: '|'
-=======
-    backSpeed: 20,
-    cursorChar: ';'
->>>>>>> parent of 0f2823e (remove all)
   };
 
   return (
     <section>
       <p className="text-xl">
-<<<<<<< HEAD
         Hello 👋, <Typing options={options} />
         {/* teste */}
-=======
-        {/* text here */}
-        <Typing options={options} />
->>>>>>> parent of 0f2823e (remove all)
       </p>
     </section>
   );
